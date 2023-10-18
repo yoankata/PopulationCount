f8f67ec38f46f3cd352be7bb72f4c9af1bece522
Yoana <yoanadim@hotmail.com>
2023-10-18 20:55:36 +0300

    Add raw sql and console prints as sanity check.

--- PopulationCount/Program.cs
+++ PopulationCount/Program.cs
@@ -1,37 +1,100 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Backend;
 using PopulationCount;
+using System.Data;
 using System.Data.Common;
 
-Console.WriteLine("Hello, World!");
 Console.WriteLine("Started");
+
 Console.WriteLine("Getting DB Connection...");
-var query = $"select CountryName, Sum(c.Population)" +
-    $"from City as c inner join State as s on s.StateId = c.StateId inner join Country co on co.CountryId = s.CountryId" +
-    $"group by CountryName";
 IDbManager db = new SqliteDbManager();
 DbConnection conn = db.getConnection();
-conn.Open();
-conn.CreateCommand().CommandText=query;
-conn.BeginTransaction();
-conn.sql
-if (conn == null)
+
+// using db connection and raw sql
+if (conn != null)
+{
+    conn.Open();
+    using (conn)
+    {
+        try
+        {
+            // Create query
+            var queryString = $"select CountryName, Sum(c.Population) " +
+                $"from City as c inner join State as s on s.StateId = c.StateId inner join Country co on co.CountryId = s.CountryId " +
+                $"group by CountryName " +
+                $"order by CountryName";
+
+            // Create the command.
+            DbCommand command = conn.CreateCommand();
+            command.CommandText = queryString;
+            command.CommandType = CommandType.Text;
+
+
+            // Retrieve the data.
+            DbDataReader reader = command.ExecuteReader();
+            Console.WriteLine($"DB raw:");
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader[0], 35}: {reader[1], 20:N0}");
+            }
+        }
+
+        catch (DbException dbex)
+        {
+            Console.WriteLine($"DbException.GetType: {dbex.GetType()}");
+            Console.WriteLine($"DbException.Source: {dbex.Source}");
+            Console.WriteLine($"DbException.ErrorCode: {dbex.ErrorCode}");
+            Console.WriteLine($"DbException.Message: {dbex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"General Exception: {ex.Message}");
+        }
+    }
+}
+else
 {
     Console.WriteLine("Failed to get connection");
 }
 
+// using EF
+var dbPopulationCount = new Dictionary<string, long>();
+using (var context = new CitystatecountryContext())
+{
+    dbPopulationCount = context.Countries
+        .Select(x => Tuple.Create(x.CountryName, x.States.Sum(y => y.Cities.Sum(z => z.Population))))
+        .ToDictionary(z => z.Item1, z => z.Item2);
+}
+
+
+Console.WriteLine($"DB EF:");
+foreach (var item in dbPopulationCount.OrderBy(o => o.Key))
+{
+
+    Console.WriteLine($"{item.Key,35}: {item.Value, 20:N0}");
+
+}
 
 var stateService = new ConcreteStatService();
-using var context = new CitystatecountryContext();
-var dbPopulationCount = context.Countries.Select(x => Tuple.Create(x.CountryName, x.States.Sum(y => y.Cities.Sum(z=> z.Population))))
-    .ToDictionary(z => z.Item1, z=>z.Item2 );
+var apiPopulationCount = (await stateService.GetCountryPopulationsAsync())
+     .ToDictionary(z => z.Item1, z => z.Item2);
 
-var apiPopulationCount = (await stateService.GetCountryPopulationsAsync()).Where(x => !dbPopulationCount.ContainsKey(x.Item1))
-     .ToDictionary(z => z.Item1, z => z.Item2).Union(dbPopulationCount);
-
-
-foreach (var item in dbPopulationCount)
+Console.WriteLine($"API:");
+foreach (var item in apiPopulationCount.OrderBy(o => o.Key))
 {
-    Console.WriteLine($"{item.Key}:\t {item.Value}");
+
+    Console.WriteLine($"{item.Key, 35}: {item.Value, 20:N0}");
+
+}
+
+var joinedPopulationCount = (await stateService.GetCountryPopulationsAsync())
+    .Where(x => !dbPopulationCount.ContainsKey(x.Item1))
+    .ToDictionary(z => z.Item1, z => z.Item2).Union(dbPopulationCount);
+
+Console.WriteLine($"DB and API Joined (DB precedence):");
+foreach (var item in joinedPopulationCount.OrderBy(o => o.Key))
+{
+
+    Console.WriteLine($"{item.Key, 35}: {item.Value, 20:N0}");
 
 }
